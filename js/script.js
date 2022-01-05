b869f8b953af8a4727ce0ab09a88c09b45ada3e8
Everette Carrera <everettecarrera@gmail.com>
2022-01-05 13:08:59 -0600

    Update script.js
    Fetch GitHub repos, list repos

--- js/script.js
+++ js/script.js
@@ -1,14 +1,16 @@
 // Profile info will appear in this class
 const overview = document.querySelector(".overview");
 const username = "eacarr";
+const repoList = document.querySelector(".repo-list");
 
 // Fetch API JSON Data
 
 const gitHubProfile = async (username) => {
   const user = await fetch(`https://api.github.com/users/${username}`);
   const userData = await user.json();
-  console.log(user);
-  console.log(userData);
+  // console.log(user);
+  // console.log(userData);
+
   // Call the display function & view on project
   displayUserInfo(userData);
 };
@@ -29,4 +31,28 @@ const displayUserInfo = (userData) => {
   <p><strong>Number of public repos:</strong> ${userData.public_repos}</p>
 </div> `;
   overview.append(div);
+  userRepos();
+};
+
+// Fetch Repos
+
+const userRepos = async () => {
+  const getRepos = await fetch(
+    `https://api.github.com/users/${username}/repos?per_page=100&per_page=100`
+  );
+  const reposData = await getRepos.json();
+  // console.log(getRepos);
+  // console.log(reposData);
+  displayRepoInfo(reposData);
+};
+
+// Display info about repos
+
+const displayRepoInfo = (repos) => {
+  repos.forEach((repo) => {
+    let li = document.createElement("li");
+    li.classList.add("reop");
+    li.innerHTML = `<h3>${repo.name}</h3>`;
+    repoList.append(li);
+  });
 };
