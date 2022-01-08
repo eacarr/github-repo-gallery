578a5ef76f71a51cc1c40032fd21204e65de27ae
Everette Carrera <everettecarrera@gmail.com>
2022-01-08 02:18:25 -0600

    Update script.js
    display input element, add input event to search box

--- js/script.js
+++ js/script.js
@@ -1,22 +1,22 @@
 // Profile info will appear in this class
 const overview = document.querySelector(".overview");
+// Other Globlal variables
+
 const username = "eacarr";
 const repoList = document.querySelector(".repo-list");
-const repos = document.querySelector(".repos");
+const reposContainer = document.querySelector(".repos");
 const repoData = document.querySelector(".repo-data");
-
+const viewRepoBtn = document.querySelector(".view-repos");
+const filterInput = document.querySelector(".filter-repos");
 // Fetch API JSON Data
 
-const gitHubProfile = async (username) => {
+const gitHubProfile = async () => {
   const user = await fetch(`https://api.github.com/users/${username}`);
   const userData = await user.json();
-  // console.log(user);
-  // console.log(userData);
-
   // Call the display function & view on project
   displayUserInfo(userData);
 };
-gitHubProfile(username);
+gitHubProfile();
 
 // Fetch & display user info
 
@@ -33,14 +33,14 @@ const displayUserInfo = (userData) => {
   <p><strong>Number of public repos:</strong> ${userData.public_repos}</p>
 </div> `;
   overview.append(div);
-  userRepos();
+  userRepos(username);
 };
 
 // Fetch Repos
 
-const userRepos = async () => {
+const userRepos = async (username) => {
   const getRepos = await fetch(
-    `https://api.github.com/users/${username}/repos?per_page=100&per_page=100`
+    `https://api.github.com/users/${username}/repos?sort=updated&per_page=100`
   );
   const reposData = await getRepos.json();
   // console.log(getRepos);
@@ -50,10 +50,11 @@ const userRepos = async () => {
 
 // Display info about repos
 
-const displayRepoInfo = (repos) => {
-  repos.forEach((repo) => {
+const displayRepoInfo = (reposData) => {
+  filterInput.classList.remove("hide");
+  reposData.forEach((repo) => {
     let li = document.createElement("li");
-    li.classList.add("reop");
+    li.classList.add("repo");
     li.innerHTML = `<h3>${repo.name}</h3>`;
     repoList.append(li);
   });
@@ -95,6 +96,11 @@ const specificRepo = async (repoName) => {
 
 const displayspecificRepo = (repoInfo, languages) => {
   repoData.innerHTML = "";
+  repoData.classList.remove("hide");
+  reposContainer.classList.add("hide");
+  // show back to repo button
+  viewRepoBtn.classList.remove("hide");
+
   const div = document.createElement("div");
   div.innerHTML = `<h3>Name: ${repoInfo.name}</h3>
   <p>Description: ${repoInfo.description}</p>
@@ -102,6 +108,29 @@ const displayspecificRepo = (repoInfo, languages) => {
   <p>Languages: ${languages}</p>
   <a class="visit" href="${repoInfo.html_url}" rel="noreferrer noopener">View Repo on GitHub!</a>`;
   repoData.append(div);
-  repoData.classList.remove("hide");
-  repos.classList.add("hide");
 };
+
+// Event listener for back to repo button
+
+viewRepoBtn.addEventListener("click", () => {
+  repoData.classList.add("hide");
+  reposContainer.classList.remove("hide");
+  viewRepoBtn.classList.add("hide");
+});
+
+// Event listener for filterinput
+
+filterInput.addEventListener("input", (e) => {
+  const inputValue = e.target.value;
+  // console.log(inputValue);
+  const repos = document.querySelectorAll(".repo");
+  const inputValueLower = inputValue.toLowerCase();
+  repos.forEach((repo) => {
+    const repoValueText = repo.innerText.toLowerCase();
+    if (repoValueText.includes(inputValueLower)) {
+      repo.classList.remove("hide");
+    } else {
+      repo.classList.add("hide");
+    }
+  });
+});
